812f3f3ebbef99c25afff4db72156cf54e4c10ee
Jordan <toulain.jordan@educ-valadon-limoges.fr>
2026-06-11 10:59:36 +0200

    Fix: Message d'import ErrorAlert.

--- web/src/components/auth/index.js
+++ web/src/components/auth/index.js
@@ -1,7 +1,7 @@
 /**
  * Exports des composants d'authentification
  */
-export { default as ErrorAlert } from '../shared'
+export { ErrorAlert } from '../shared'
 export { default as AuthCard } from './AuthCard'
 export { default as FormInput } from './FormInput'
 export { default as FormButton } from './FormButton'
