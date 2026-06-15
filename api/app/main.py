299a8b511d43508276afa50c10bc82c54b0a1ae4
Jordan <toulain.jordan@educ-valadon-limoges.fr>
2026-06-15 15:05:38 +0200

    Debug: Ajout logger Phoenix.

--- api/app/main.py
+++ api/app/main.py
@@ -3,10 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.router import api_router
 from core.supabase_init import init_db
 from contextlib import asynccontextmanager
+import llama_index.core
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
+    
+    llama_index.core.set_global_handler("arize_phoenix", 
+        endpoint="http://phoenix:6006/v1/traces")
+    
     yield
 
 app = FastAPI(lifespan=lifespan)
